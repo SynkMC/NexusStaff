fb7a0b56a63479ececfb1da507a6d7d45402e61e
Synk_ <81869570+SynkMC@users.noreply.github.com>
2025-06-09 15:33:50 +0200

    Project init

---
+++ src/main/java/cc/synkdev/nexusStaff/NexusStaff.java
@@ -0,0 +1,17 @@
+package cc.synkdev.nexusStaff;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NexusStaff extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
